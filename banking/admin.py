6c24e548a45e1b26b662759daeddfeca7a9a2598
Akash Chauhan <akash.chauhan@dignitech.com>
2020-11-28 23:11:18 +0530

    removed delete option in address admin

--- banking/admin.py
+++ banking/admin.py
@@ -17,3 +17,6 @@ class AddressAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request, obj=None):
         return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
